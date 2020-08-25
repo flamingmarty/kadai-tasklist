ca38d47fd8799249446b854d93b3bccbe86d38bd
flaminig.marty <flaming.marty@gmail.com>
2020-08-25 12:54:27 +0000

    modify before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
@@ -7,7 +6,7 @@ class TasksController < ApplicationController
       #@tasks = Task.all
       
       if logged_in?
-        @task = current_user.tasks.build  # form_with 用
+        #@task = current_user.tasks.build  # form_with 用
         @tasks = current_user.tasks.order(id: :asc).page(params[:page]).per(5)
       end
   end
@@ -21,6 +20,7 @@ class TasksController < ApplicationController
 
   def create
     #@task = Task.new(task_params)
+    
     @task = current_user.tasks.build(task_params)
 
     if @task.save
@@ -54,10 +54,6 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
